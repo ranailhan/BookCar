d16933eeebeddecb70aa8e566172c19947ee7e16
ranailhan <rana.ilhan05@gmail.com>
2026-05-01 02:24:43 +0300

    Add Contact CQRS handlers, commands, queries and API controller

--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -4,6 +4,7 @@ using CarBook.Application.Features.CQRS.Handlers.BannerHandlers;
 using CarBook.Application.Features.CQRS.Handlers.BrandHandlers;
 using CarBook.Application.Features.CQRS.Handlers.CarHandlers;
 using CarBook.Application.Features.CQRS.Handlers.CategoryHandlers;
+using CarBook.Application.Features.CQRS.Handlers.ContactHandlers;
 using CarBook.Application.Interfaces;
 using CarBook.Application.Interfaces.CarInterfaces;
 using CarBook.Persistence.Context;
@@ -55,6 +56,12 @@ namespace CarBook.WebApi
             builder.Services.AddScoped<CreateCategoryCommandHandler>();
             builder.Services.AddScoped<UpdateCategoryCommandHandler>();
             builder.Services.AddScoped<RemoveCategoryCommandHandler>();
+
+            builder.Services.AddScoped<GetContactQueryHandler>();
+            builder.Services.AddScoped<GetContactByIdQueryHandler>();
+            builder.Services.AddScoped<CreateContactCommandHandler>();
+            builder.Services.AddScoped<UpdateContactCommandHandler>();
+            builder.Services.AddScoped<RemoveContactCommandHandler>();
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
